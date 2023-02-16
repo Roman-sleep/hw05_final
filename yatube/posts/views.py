cfe4d5a0a5f17d8d14c20892c4e37187dd4b16a5
Роман Поляков <Roman06182000@yandex.ru>
2023-02-16 11:41:42 +0300

    now_posts_views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post, Group, User, Follow
+from .models import Post, Group, User, Follow, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -7,20 +7,25 @@ from yatube.settings import POSTS_PER_PAGE
 
 
 def paginator(page_number, posts):
+    """Вспомогательная функция для паджинатора."""
     paginator = Paginator(posts, POSTS_PER_PAGE)
     page_obj = paginator.get_page(page_number)
     return page_obj
 
 
 def index(request):
+    """Главная страница."""
+    template = 'posts/index.html'
     posts = Post.objects.all()
     context = {
         'page_obj': paginator(request.GET.get('page'), posts),
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
+    """Страница постов группы."""
+    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group)
     context = {
@@ -28,11 +33,13 @@ def group_posts(request, slug):
         'posts': posts,
         'page_obj': paginator(request.GET.get('page'), posts),
     }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, template, context)
 
 
 def profile(request, username):
-    author = get_object_or_404(User, username=username)
+    """Страница пользователя."""
+    author = get_object_or_404(
+        User.objects.select_related('profile'), username=username)
     posts = author.posts.all()
     template = 'posts/profile.html'
     context = {
@@ -44,6 +51,7 @@ def profile(request, username):
 
 
 def post_detail(request, post_id):
+    """Информация о посте."""
     form = CommentForm(request.POST or None)
     page_obj = get_object_or_404(Post, id=post_id)
     comments = page_obj.comments.all()
@@ -58,6 +66,8 @@ def post_detail(request, post_id):
 
 @login_required
 def post_create(request):
+    """Создание поста."""
+    template = 'posts/create_post.html'
     form = PostForm(
         request.POST or None,
         files=request.FILES or None
@@ -67,12 +77,14 @@ def post_create(request):
         post.author = request.user
         post.save()
         return redirect('posts:profile', post.author.username)
-    return render(request, 'posts/create_post.html', {'form': form})
+    return render(request, template, {'form': form})
 
 
 @login_required
 def post_edit(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+    """Редактирование поста."""
+    template = 'posts/create_post.html'
+    post = get_object_or_404(Post.objects.select_related('author'), pk=post_id)
     if post.author != request.user:
         return redirect('posts:post_detail', post.id)
     form = PostForm(
@@ -88,11 +100,22 @@ def post_edit(request, post_id):
         'form': form,
         'post': post
     }
-    return render(request, 'posts/create_post.html', context)
+    return render(request, template, context)
+
+
+@login_required
+def post_del(request, post_id):
+    """Удаление поста."""
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user.pk != post.author.pk:
+        return redirect('posts:index')
+    post.delete()
+    return redirect('posts:index')
 
 
 @login_required
 def add_comment(request, post_id):
+    """Создание комментария к посту."""
     post = get_object_or_404(Post, id=post_id)
     form = CommentForm(request.POST or None)
     if form.is_valid():
@@ -104,20 +127,53 @@ def add_comment(request, post_id):
 
 
 @login_required
-def follow_index(request):
-    posts = Post.objects.filter(author__following__user=request.user)
-    paginator = Paginator(posts, POSTS_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+def edit_comment(request, comment_id):
+    """Редактирование коммента."""
+    template = 'posts/edit_comment.html'
+    comment = get_object_or_404(
+        Comment.objects.select_related('author'), pk=comment_id
+    )
+    if request.user.pk != comment.author.pk:
+        return redirect('posts:post_detail', comment.post.pk)
+    form = CommentForm(
+        instance=comment,
+        data=request.POST or None,
+    )
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', comment.post.pk)
     context = {
-        'page_obj': page_obj,
+        'form': form,
+        'title': 'Редактировать коммент'
+    }
+    return render(request, template, context)
+
+
+@login_required
+def del_comment(request, comment_id):
+    """Удаление коммента."""
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user.pk != comment.author.pk:
+        return redirect('posts:post_detail', comment.post.pk)
+    comment.delete()
+    return redirect('posts:post_detail', comment.post.pk)
+
+
+@login_required
+def follow_index(request):
+    """Подписки пользователя."""
+    template = 'posts/follow.html'
+    posts = Post.objects.filter(author__following__user=request.user)
+    context = {
+        'page_obj': paginator(request.GET.get('page'), posts),
         'title': 'Подписки'
     }
-    return render(request, 'posts/follow.html', context)
+    return render(request, template, context)
 
 
 @login_required
 def profile_follow(request, username):
+    """Подписка на автора."""
     user = request.user
     author = get_object_or_404(User, username=username)
     is_follower = Follow.objects.filter(user=user, author=author)
@@ -128,6 +184,7 @@ def profile_follow(request, username):
 
 @login_required
 def profile_unfollow(request, username):
+    """Отписка от автора."""
     author = get_object_or_404(User, username=username)
     is_follower = Follow.objects.filter(user=request.user, author=author)
     is_follower.delete()
