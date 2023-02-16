17973292d841404388fd8fe41df33177ce366d8c
Роман Поляков <Roman06182000@yandex.ru>
2023-02-16 11:44:02 +0300

    now_posts_urls

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -10,8 +10,19 @@ urlpatterns = [
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('create/', views.post_create, name='post_create'),
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('posts/<int:post_id>/del/', views.post_del, name='post_del'),
     path('posts/<int:post_id>/comment/',
          views.add_comment, name='add_comment'),
+    path(
+        'posts/edit_comment/<int:comment_id>/',
+        views.edit_comment,
+        name='edit_comment'
+    ),
+    path(
+        'posts/del_comment/<int:comment_id>/',
+        views.del_comment,
+        name='del_comment'
+    ),
     path('follow/', views.follow_index, name='follow_index'),
     path(
         'profile/<str:username>/follow/',
